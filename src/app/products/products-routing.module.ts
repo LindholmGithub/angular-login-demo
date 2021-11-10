61db030a2dc805e1ba16a8c343f46c4b6ec9c5ee
Lars Juul <ljb@easv.dk>
2021-11-10 12:30:13 +0100

    Adding more

--- src/app/products/products-routing.module.ts
+++ src/app/products/products-routing.module.ts
@@ -1,8 +1,10 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import {ProductsComponent} from './list/products.component';
+import {UpdateComponent} from './update/update.component';
 
 const routes: Routes = [
+  {path: 'update/:id', component: UpdateComponent},
   {path: '', component: ProductsComponent}
 ];
 
--- src/app/products/products.module.ts
+++ src/app/products/products.module.ts
@@ -3,10 +3,12 @@ import { CommonModule } from '@angular/common';
 import { ProductsRoutingModule } from './products-routing.module';
 import { ProductsComponent } from './list/products.component';
 import {HttpClientModule} from '@angular/common/http';
+import { UpdateComponent } from './update/update.component';
 
 @NgModule({
   declarations: [
-    ProductsComponent
+    ProductsComponent,
+    UpdateComponent
   ],
   imports: [
     CommonModule,
